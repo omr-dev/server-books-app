e2fca68ee5f604f8f6dbcbad67f747613f29df13
omer <peynirciomerfaruk@gmail.com>
2022-05-28 14:57:50 +0200

    data added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import data from "./data.js";
 
 const app = express();
 const PORT = 5000; //TODO: fix for heroku
 
 app.get("/", (req, res) => {
-  return res.json({ id: 1, title: "This is a test object" });
+  return res.json(data);
 });
 app.listen(PORT, () => {
   console.log(`Books server running on http://localhost:${PORT}`);
