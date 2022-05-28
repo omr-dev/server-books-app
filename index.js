f9ad96526c8c9f4437414f9431bdeea6774a8e43
omer <peynirciomerfaruk@gmail.com>
2022-05-28 15:13:21 +0200

    fixed log

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
   return res.json(data);
 });
 app.listen(PORT, () => {
-  console.log(`Books server running on http://localhost:${PORT}`);
+  console.log(`Books server running successfully.`);
 });
